9040f3e875821fec99ed1523ee35dd128c8a3555
maksklimko <maks.klmqaz@gmail.com>
2021-10-28 22:17:37 +0300

    w

--- index.js
+++ index.js
@@ -1,63 +1,20 @@
-// Импорт Telegraf и Markup
-const {
-  Telegraf,
-  Markup
-} = require('telegraf')
-// Импорт dotenv для защиты API токена
-require('dotenv').config()
-// Импорт нашего модуля с константами
-const my_const = require('./const')
-// Инициализация бота с помощью Telegraf
-const bot = new Telegraf("2084934381:AAF6WJkofwxx4kqd7JHYRh8dTwpIIlZbIo8")
+const TelegramBot = require('node-telegram-bot-api');
 
-// Обработка команды /start
-bot.start((ctx) => ctx.reply(`Привет ${ctx.message.from.first_name ? ctx.message.from.first_name : 'незнакомец'}!`))
-// Обработка команды /help
-bot.help((ctx) => ctx.reply(my_const.commands))
-// Обработка команды /course
-bot.command('course', async (ctx) => {
-  try {
-    await ctx.replyWithHTML('<b>Курсы</b>', Markup.inlineKeyboard(
-      [
-        [Markup.button.callback('Редакторы', 'btn_1'), Markup.button.callback('Обзоры', 'btn_2'), Markup.button.callback('JS', 'btn_3')]
-      ]
-    ))
-  } catch (e) {
-    console.error(e)
-  }
-})
-/**
- * Функция для отправки сообщения ботом
- * @param {String} id_btn Идентификатор кнопки для обработки
- * @param {String} src_img Путь к изображению, или false чтобы отправить только текст
- * @param {String} text Текстовое сообщение для отправки
- * @param {Boolean} preview Блокировать превью у ссылок или нет, true - блокировать, false - нет
- */
-function addActionBot(id_btn, src_img, text, preview) {
-  bot.action(id_btn, async (ctx) => {
-    try {
-      await ctx.answerCbQuery()
-      if (src_img !== false) {
-        await ctx.replyWithPhoto({
-          source: src_img
-        })
-      }
-      await ctx.replyWithHTML(text, {
-        disable_web_page_preview: preview
-      })
-    } catch (e) {
-      console.error(e)
+const token = '2084934381:AAF6WJkofwxx4kqd7JHYRh8dTwpIIlZbIo8';
+
+const bot = new TelegramBot(token, {polling: true});
+bot.setMyCommands([
+    {command: '/create', description: 'Створити опитування'},
+]);
+
+bot.on('message', (msg) => {
+    const text = msg.text;
+    const chatId = msg.chat.id;
+
+    if(text==="/create"){
+    if(text==="/create@VolleyballPullSnippetsBot"||text==="/create"){
+      bot.setMyCommands() 
+      bot.sendPoll(chatId, "Волейбол?",["+","-"],{"is_anonymous":"false"});
+      bot.sendPoll(chatId, "Година?",["5","6","7","8","Без різниці"],{"is_anonymous":"false","allows_multiple_answers":"true"});
     }
-  })
-}
-// Обработчик кнопок с помощью функции
-addActionBot('btn_1', './img/1.jpg', my_const.text1, true)
-addActionBot('btn_2', './img/2.jpg', my_const.text2, true)
-addActionBot('btn_3', false, my_const.text3, false)
-
-// Запустить бота
-bot.launch()
-
-// Включить плавную остановку
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+}});
