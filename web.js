9e6a0fe32869ed5503bcda0fa8e7cf76d1e55ad6
maksklimko <maks.klmqaz@gmail.com>
2021-10-28 20:28:59 +0300

    Update web.js

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.json({ version: packageInfo.version });
 });
 
-var server = app.listen(process.env.PORT, function () {
+var server = app.listen(5000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
