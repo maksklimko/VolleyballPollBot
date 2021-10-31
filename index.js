d894b659eee46267285c4e46eff6ec4b3c4b72da
maksklimko <maks.klmqaz@gmail.com>
2021-10-31 10:20:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const TelegramBot = require('node-telegram-bot-api');
 
+let arr = [];
+let chatId;
+
 const rule = new schedule.RecurrenceRule();
 rule.dayOfWeek = [0,3,5];
 rule.hour = 0;
@@ -18,13 +21,19 @@ bot.on('message', (msg) => {
    
     if(text==="/create@VolleyballPullSnippetsBot"||text==="/create"){
       bot.sendPoll(chatId, "Волейбол?",["+","-"],{"is_anonymous":"false"}).then(msg=>{
-          bot.deleteMessage(msg.chat.id,msg.id);
+          chatId=this.chatId;
+          arr.push(msg.id);
       });
       bot.sendPoll(chatId, "Година?",["5","6","7","8","Без різниці"],{"is_anonymous":"false","allows_multiple_answers":"true"});
+    }else{
+      for (let i = 0; i < arr.length; i++) {
+        bot.deleteMessage(msg.chat.id,arr[i]);
+      }
     }
 });
 
 const job = schedule.scheduleJob(rule, function(){
-  bot.sendPoll(chatId, "Волейбол?",["+","-"],{"is_anonymous":"false"}).then();
+      
+      bot.sendPoll(chatId, "Волейбол?",["+","-"],{"is_anonymous":"false"});
       bot.sendPoll(chatId, "Година?",["5","6","7","8","Без різниці"],{"is_anonymous":"false","allows_multiple_answers":"true"});
 });
